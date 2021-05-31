807592b962592e71a5d6cc345f71d789cda881fa
graythze <54765502+graythze@users.noreply.github.com>
2021-05-31 13:40:39 +0500

    Update methods

--- collector.py
+++ collector.py
@@ -11,30 +11,30 @@ def timer():
     time.sleep(0.36)
 
 
-data = {'id': user_id,
-        'parsing_started': int(time.time()),
-        'main_profile': methods.users_get(user_id, token, v),
-        'wall': methods.wall_get(user_id, token, v),
-        'documents': methods.docs_get(user_id, token, v),
-        'photos': methods.photos_get_all(user_id, token, v),
-        'notes': methods.notes_get(user_id, token, v),
-        'videos': methods.videos_get(user_id, token, v),
-        'friends': methods.friends_get(user_id, token, v),
-        'gifts': methods.gifts_get(user_id, token, v),
-        'stories': methods.stories_get(user_id, token, v),
-        'groups': methods.groups_get(user_id, token, v),
-        'market': methods.market_get(user_id, token, v),
-        'parsing_finished': int(time.time())}
+data = {"id": user_id,
+        "parsing_started": int(time.time()),
+        "main_profile": methods.users_get(user_id, token, v),
+        "wall": methods.wall_get(user_id, token, v),
+        "documents": methods.docs_get(user_id, token, v),
+        "photos": methods.photos_get_all(user_id, token, v),
+        "notes": methods.notes_get(user_id, token, v),
+        "videos": methods.videos_get(user_id, token, v),
+        "friends": methods.friends_get(user_id, token, v),
+        "gifts": methods.gifts_get(user_id, token, v),
+        "stories": methods.stories_get(user_id, token, v),
+        "groups": methods.groups_get(user_id, token, v),
+        "market": methods.market_get(user_id, token, v),
+        "parsing_finished": int(time.time())}
 
 
-''' be careful with followers_get. don't use it if you don't want to parse ALL FOLLOWERS '''
-# data['followers'] = methods.followers_get(user_id, token, v)
+""" be careful with followers_get. don't use it if you don't want to parse ALL FOLLOWERS """
+# data["followers"] = methods.followers_get(user_id, token, v)
 
-''' be careful with messages_get. don't use it if you don't want to parse OWN MESSAGES '''
-# data['messages'] = methods.messages_get(token, v)
+""" be careful with messages_get. don't use it if you don't want to parse OWN MESSAGES """
+# data["messages"] = methods.messages_get(token, v)
 
 
-with open('export' + str(user_id) + "_" + str(int(time.time())) + ".json", mode='w', encoding='utf-8') as file:
+with open("export" + str(user_id) + "_" + str(int(time.time())) + ".json", mode="w", encoding="utf-8") as file:
     file.write(str(data))
 
 
--- methods.py
+++ methods.py
@@ -11,14 +11,14 @@ def get_numeric_id(id, token, v):
         return id
     else:
         request = requests.post("https://api.vk.com/method/users.get", data={
-            'user_ids': id,
-            'access_token': token,
-            'v': v}).json()
+            "user_ids": id,
+            "access_token": token,
+            "v": v}).json()
         if "response" in request:
             timer()
-            return request['response'][0]['id']
+            return request["response"][0]["id"]
         else:
-            exit('ERROR CODE ' + str(request["error"]["error_code"]) + ': ' + request["error"]["error_msg"])
+            exit("ERROR CODE " + str(request["error"]["error_code"]) + ": " + request["error"]["error_msg"])
 
 
 def docs_get(id, token, v):
@@ -26,19 +26,20 @@ def docs_get(id, token, v):
     requests_all = []
     while True:
         request = requests.post("https://api.vk.com/method/docs.get", data={
-            'count': 2000,
-            'offset': offset,
-            'owner_id': id,
-            'return_tags': 1,
-            'access_token': token,
-            'v': v}).json()
+            "count": 2000,
+            "offset": offset,
+            "owner_id": id,
+            "return_tags": 1,
+            "access_token": token,
+            "v": v}).json()
         print(request)
         if "response" in request and len(request["response"]["items"]) > 0:
-            requests_all.append(request["response"])
+            for k in request["response"]["items"]:
+                requests_all.append(k)
             offset += 1999
             timer()
         else:
-            print('nothing to parse')
+            print("nothing to parse")
             timer()
             break
     return requests_all
@@ -49,25 +50,27 @@ def friends_get(id, token, v):
     requests_all = []
     while True:
         request = requests.post("https://api.vk.com/method/friends.get", data={
-            'user_id': id,
-            'order': 'name',
-            'count': 5000,
-            'offset': offset,
-            'fields': 'uid,first_name,last_name,deactivated,verified,sex,bdate,city,country,home_town,photo_50,'
-                      'photo_100,photo_200_orig,photo_200,photo_400_orig,photo_max,photo_max_orig,online,lists,'
-                      'domain,has_mobile,contacts,site,education,universities,schools,status,last_seen,'
-                      'followers_count,counters,occupation,nickname,relatives,relation,personal,connections,exports,'
-                      'wall_comments,activities,interests,music,movies,tv,books,games,about,quotes,can_post,'
-                      'can_see_all_posts,can_see_audio,can_write_private_message,timezone,screen_name',
-            'access_token': token,
-            'v': v}).json()
+            "user_id": id,
+            "order": "name",
+            "count": 5000,
+            "offset": offset,
+            "fields": "uid,first_name,last_name,deactivated,verified,sex,bdate,city,country,home_town,photo_50,"
+                      "photo_100,photo_200_orig,photo_200,photo_400_orig,photo_max,photo_max_orig,online,lists,"
+                      "domain,has_mobile,contacts,site,education,universities,schools,status,last_seen,"
+                      "followers_count,counters,occupation,nickname,relatives,relation,personal,connections,exports,"
+                      "wall_comments,activities,interests,music,movies,tv,books,games,about,quotes,can_post,"
+                      "can_see_all_posts,can_see_audio,can_write_private_message,timezone,screen_name",
+            "access_token": token,
+            "v": v}).json()
         print(request)
         if "response" in request and len(request["response"]["items"]) > 0:
-            requests_all.append(request["response"])
+            for k in request["response"]["items"]:
+                del k["track_code"]
+                requests_all.append(k)
             offset += 5000
             timer()
         else:
-            print('nothing to parse')
+            print("nothing to parse")
             timer()
             break
     return requests_all
@@ -78,18 +81,20 @@ def gifts_get(id, token, v):
     requests_all = []
     while True:
         request = requests.post("https://api.vk.com/method/gifts.get", data={
-            'user_id': id,
-            'count': 1000,
-            'offset': offset,
-            'access_token': token,
-            'v': v}).json()
+            "user_id": id,
+            "count": 1000,
+            "offset": offset,
+            "access_token": token,
+            "v": v}).json()
         print(request)
         if "response" in request and len(request["response"]["items"]) > 0:
-            requests_all.append(request["response"])
+            for k in request["response"]["items"]:
+                del k['gift_hash']
+                requests_all.append(k)
             offset += 1000
             timer()
         else:
-            print('nothing to parse')
+            print("nothing to parse")
             timer()
             break
     return requests_all
@@ -100,19 +105,19 @@ def notes_get(id, token, v):
     requests_all = []
     while True:
         request = requests.post("https://api.vk.com/method/notes.get", data={
-            'user_id': id,
-            'offset': offset,
-            'count': 100,
-            'sort': 1,
-            'access_token': token,
-            'v': v}).json()
+            "user_id": id,
+            "offset": offset,
+            "count": 100,
+            "sort": 1,
+            "access_token": token,
+            "v": v}).json()
         print(request)
         if "response" in request and len(request["response"]["items"]) > 0:
             requests_all.append(request["response"])
             offset += 100
             timer()
         else:
-            print('nothing to parse')
+            print("nothing to parse")
             timer()
             break
     return requests_all
@@ -123,21 +128,21 @@ def photos_get_all(id, token, v):
     requests_all = []
     while True:
         request = requests.post("https://api.vk.com/method/photos.getAll", data={
-            'owner_id': id,
-            'extended': 1,
-            'offset': offset,
-            'count': 200,
-            'photo_sizes': 1,
-            'no_service_albums': 0,
-            'access_token': token,
-            'v': v}).json()
+            "owner_id": id,
+            "extended": 1,
+            "offset": offset,
+            "count": 200,
+            "photo_sizes": 1,
+            "no_service_albums": 0,
+            "access_token": token,
+            "v": v}).json()
         print(request)
         if "response" in request and len(request["response"]["items"]) > 0:
             requests_all.append(request["response"])
             offset += 200
             timer()
         else:
-            print('nothing to parse')
+            print("nothing to parse")
             timer()
             break
     return requests_all
@@ -147,17 +152,17 @@ def stories_get(id, token, v):
     requests_all = []
     while True:
         request = requests.post("https://api.vk.com/method/stories.get", data={
-            'owner_id': id,
-            'extended': 1,
-            'access_token': token,
-            'v': v}).json()
+            "owner_id": id,
+            "extended": 1,
+            "access_token": token,
+            "v": v}).json()
         print(request)
         if "response" in request and len(request["response"]["items"]) > 0:
             requests_all.append(request["response"])
             timer()
             break
         else:
-            print('nothing to parse')
+            print("nothing to parse")
             timer()
             break
     return requests_all
@@ -165,19 +170,19 @@ def stories_get(id, token, v):
 
 def users_get(id, token, v):
     request = requests.post("https://api.vk.com/method/users.get", data={
-        'user_ids': id,
-        'fields': 'uid,first_name,last_name,deactivated,verified,sex,bdate,city,country,home_town,photo_50,photo_100,'
-                  'photo_200_orig,photo_200,photo_400_orig,photo_max,photo_max_orig,online,lists,domain,has_mobile,'
-                  'contacts,site,education,universities,schools,status,last_seen,followers_count,counters,occupation,'
-                  'nickname,relatives,relation,personal,connections,exports,wall_comments,activities,interests,music,'
-                  'movies,tv,books,games,about,quotes,can_post,can_see_all_posts,can_see_audio,'
-                  'can_write_private_message,timezone,screen_name',
-        'access_token': token,
-        'v': v}).json()
+        "user_ids": id,
+        "fields": "uid,first_name,last_name,deactivated,verified,sex,bdate,city,country,home_town,photo_50,photo_100,"
+                  "photo_200_orig,photo_200,photo_400_orig,photo_max,photo_max_orig,online,lists,domain,has_mobile,"
+                  "contacts,site,education,universities,schools,status,last_seen,followers_count,counters,occupation,"
+                  "nickname,relatives,relation,personal,connections,exports,wall_comments,activities,interests,music,"
+                  "movies,tv,books,games,about,quotes,can_post,can_see_all_posts,can_see_audio,"
+                  "can_write_private_message,timezone,screen_name",
+        "access_token": token,
+        "v": v}).json()
     print(request)
     if "response" in request:
         timer()
-        return request
+        return request["response"]
 
 
 def videos_get(id, token, v):
@@ -185,19 +190,19 @@ def videos_get(id, token, v):
     requests_all = []
     while True:
         request = requests.post("https://api.vk.com/method/video.get", data={
-            'owner_id': id,
-            'count': 200,
-            'offset': offset,
-            'extended': 1,
-            'access_token': token,
-            'v': v}).json()
+            "owner_id": id,
+            "count": 200,
+            "offset": offset,
+            "extended": 1,
+            "access_token": token,
+            "v": v}).json()
         print(request)
         if "response" in request and len(request["response"]["items"]) > 0:
             requests_all.append(request["response"])
             offset += 200
             timer()
         else:
-            print('nothing to parse')
+            print("nothing to parse")
             timer()
             break
     return requests_all
@@ -208,24 +213,24 @@ def followers_get(id, token, v):
     requests_all = []
     while True:
         request = requests.post("https://api.vk.com/method/users.getFollowers", data={
-            'user_id': id,
-            'offset': offset,
-            'count': 1000,
-            'fields': 'uid,first_name,last_name,deactivated,verified,sex,bdate,city,country,home_town,photo_50,'
-                      'photo_100,photo_200_orig,photo_200,photo_400_orig,photo_max,photo_max_orig,online,lists,'
-                      'domain,has_mobile,contacts,site,education,universities,schools,status,last_seen,'
-                      'followers_count,counters,occupation,nickname,relatives,relation,personal,connections,exports,'
-                      'wall_comments,activities,interests,music,movies,tv,books,games,about,quotes,can_post,'
-                      'can_see_all_posts,can_see_audio,can_write_private_message,timezone,screen_name',
-            'access_token': token,
-            'v': v}).json()
+            "user_id": id,
+            "offset": offset,
+            "count": 1000,
+            "fields": "uid,first_name,last_name,deactivated,verified,sex,bdate,city,country,home_town,photo_50,"
+                      "photo_100,photo_200_orig,photo_200,photo_400_orig,photo_max,photo_max_orig,online,lists,"
+                      "domain,has_mobile,contacts,site,education,universities,schools,status,last_seen,"
+                      "followers_count,counters,occupation,nickname,relatives,relation,personal,connections,exports,"
+                      "wall_comments,activities,interests,music,movies,tv,books,games,about,quotes,can_post,"
+                      "can_see_all_posts,can_see_audio,can_write_private_message,timezone,screen_name",
+            "access_token": token,
+            "v": v}).json()
         print(request)
         if "response" in request and len(request["response"]["items"]) > 0:
             requests_all.append(request["response"])
             offset += 1000
             timer()
         else:
-            print('nothing to parse')
+            print("nothing to parse")
             timer()
             break
     return requests_all
@@ -236,24 +241,24 @@ def groups_get(id, token, v):
     requests_all = []
     while True:
         request = requests.post("https://api.vk.com/method/groups.get", data={
-            'user_id': id,
-            'extended': 1,
-            'fields': 'id,name,screen_name,is_closed,deactivated,is_admin,admin_level,is_member,invited_by,type,'
-                      'has_photo,photo_50,photo_100,photo_200,activity,age_limits,can_create_topic,can_message,'
-                      'can_post,can_see_all_posts,can_upload_doc,can_upload_video,city,contacts,counters,country,'
-                      'cover,description,fixed_post,main_album_id,main_section,market,members_count,place,'
-                      'public_date_label,site,status,trending,verified,wiki_page',
-            'offset': offset,
-            'count': 1000,
-            'access_token': token,
-            'v': v}).json()
+            "user_id": id,
+            "extended": 1,
+            "fields": "id,name,screen_name,is_closed,deactivated,is_admin,admin_level,is_member,invited_by,type,"
+                      "has_photo,photo_50,photo_100,photo_200,activity,age_limits,can_create_topic,can_message,"
+                      "can_post,can_see_all_posts,can_upload_doc,can_upload_video,city,contacts,counters,country,"
+                      "cover,description,fixed_post,main_album_id,main_section,market,members_count,place,"
+                      "public_date_label,site,status,trending,verified,wiki_page",
+            "offset": offset,
+            "count": 1000,
+            "access_token": token,
+            "v": v}).json()
         print(request)
         if "response" in request and len(request["response"]["items"]) > 0:
             requests_all.append(request["response"])
             offset += 1000
             timer()
         else:
-            print('nothing to parse')
+            print("nothing to parse")
             timer()
             break
     return requests_all
@@ -261,30 +266,30 @@ def groups_get(id, token, v):
 
 def messages_get(token, v):
     count = 0
-    ids = ''
+    ids = ""
     requests_all = []
     while True:
         for k in range(count, 100 + count):
-            ids += str(k) + ','
+            ids += str(k) + ","
         request = requests.post("https://api.vk.com/method/messages.getById", data={
-            'message_ids': ids,
-            'extended': 1,
-            'fields': 'uid,first_name,last_name,deactivated,verified,sex,bdate,city,country,home_town,photo_50,photo_100,'
-                      'photo_200_orig,photo_200,photo_400_orig,photo_max,photo_max_orig,online,lists,domain,has_mobile,'
-                      'contacts,site,education,universities,schools,status,last_seen,followers_count,counters,occupation,'
-                      'nickname,relatives,relation,personal,connections,exports,wall_comments,activities,interests,music,'
-                      'movies,tv,books,games,about,quotes,can_post,can_see_all_posts,can_see_audio,'
-                      'can_write_private_message,timezone,screen_name',
-            'access_token': token,
-            'v': v}).json()
+            "message_ids": ids,
+            "extended": 1,
+            "fields": "uid,first_name,last_name,deactivated,verified,sex,bdate,city,country,home_town,photo_50,photo_100,"
+                      "photo_200_orig,photo_200,photo_400_orig,photo_max,photo_max_orig,online,lists,domain,has_mobile,"
+                      "contacts,site,education,universities,schools,status,last_seen,followers_count,counters,occupation,"
+                      "nickname,relatives,relation,personal,connections,exports,wall_comments,activities,interests,music,"
+                      "movies,tv,books,games,about,quotes,can_post,can_see_all_posts,can_see_audio,"
+                      "can_write_private_message,timezone,screen_name",
+            "access_token": token,
+            "v": v}).json()
         print(request)
         if "response" in request and len(request["response"]["items"]) > 0:
             requests_all.append(request["response"])
-            ids = ''
+            ids = ""
             count += 100
             timer()
         else:
-            print('nothing to parse')
+            print("nothing to parse")
             timer()
             break
         print(ids)
@@ -296,20 +301,20 @@ def wall_get(id, token, v):
     requests_all = []
     while True:
         request = requests.post("https://api.vk.com/method/wall.get", data={
-            'owner_id': id,
-            'offset': offset,
-            'count': 100,
-            'filter': 'all',
-            'extended': 1,
-            'access_token': token,
-            'v': v}).json()
+            "owner_id": id,
+            "offset": offset,
+            "count": 100,
+            "filter": "all",
+            "extended": 1,
+            "access_token": token,
+            "v": v}).json()
         print(request)
         if "response" in request and len(request["response"]["items"]) > 0:
             requests_all.append(request["response"])
             offset += 100
             timer()
         else:
-            print('nothing to parse')
+            print("nothing to parse")
             timer()
             break
     return requests_all
@@ -320,19 +325,19 @@ def market_get(id, token, v):
     requests_all = []
     while True:
         request = requests.post("https://api.vk.com/method/market.get", data={
-            'owner_id': id,
-            'count': 200,
-            'offset': offset,
-            'extended': 1,
-            'access_token': token,
-            'v': v}).json()
+            "owner_id": id,
+            "count": 200,
+            "offset": offset,
+            "extended": 1,
+            "access_token": token,
+            "v": v}).json()
         print(request)
         if "response" in request and len(request["response"]["items"]) > 0:
             requests_all.append(request["response"])
             offset += 200
             timer()
         else:
-            print('nothing to parse')
+            print("nothing to parse")
             timer()
             break
     return requests_all
